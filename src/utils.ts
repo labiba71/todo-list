9a6797360951bd38b82b52453576e67d82855c24
Labiba Ibnat <labiba.ssp@gmail.com>
2021-03-10 18:39:59 +0600

    utils func added

---
+++ src/utils.ts
@@ -0,0 +1,9 @@
+export const Color = [
+  { code: 0, value: '#FFFFFF' },
+  { code: 1, value: '#01D4FF' },
+  { code: 2, value: '#FF9B73' },
+  { code: 3, value: '#B692FE' },
+  { code: 4, value: '#E4EE90' },
+  { code: 5, value: '#FFC971' },
+
+];
